7a9e94c68d26e091e3d4b0f5de9bf661385046be
Sanborn <sanbornh@rogers.com>
2014-05-12 20:06:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WalkervillesSite::Application.config.secret_key_base = '85a047088d7034cf42ad96f0068b809afc4db494519a7cae911789e04d91259e994731df573bf40f0b778b0209fc46f646b8c69d2c3711d2cf66ff335533c09e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WalkervillesSite::Application.config.session_store :cookie_store, key: '_Walkervilles_site_session'
