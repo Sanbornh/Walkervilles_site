c8a00fed70dd6095f497ab3d7e3e56f505124c40
Sanborn <sanbornh@rogers.com>
2014-09-24 17:36:27 -0400

    Updated to basic album launch site

--- Gemfile
+++ Gemfile
@@ -50,3 +50,5 @@ group :development, :test do
 end
 
 gem 'rails_12factor', group: :production
+
+gem 'browser'
--- app/controllers/walkervilles_controller.rb
+++ app/controllers/walkervilles_controller.rb
@@ -2,7 +2,7 @@ class WalkervillesController < ApplicationController
 
 	def index
 		schedule = Schedule.new
-		# @tour_dates = schedule.tour_dates
-		# binding.pry
+		@tour_dates = schedule.tour_dates
+    # binding.pry
 	end
 end
